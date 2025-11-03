a3496890b8e0560e384df49f14e056a2d44441a5
Hungerbar <3485509660@qq.com>
2025-11-03 16:52:53 +0800

    reset the Latex engine as Xelatex

--- lua/hungerbar/plugins/vimtex.lua
+++ lua/hungerbar/plugins/vimtex.lua
@@ -1,32 +1,34 @@
 return {
-  "lervag/vimtex",
-  lazy = false,
-  config = function()
-    vim.g.vimtex_quickfix_mode = 2
-    
-    -- 修正编译器配置
-    vim.g.vimtex_compiler_latexmk = {
-      build_dir = 'build',  -- 使用正确的 build_dir 参数
-      options = {
-        '-synctex=1',
-        '-interaction=nonstopmode',
-      },
-      engine = 'xelatex',  -- 正确设置引擎
-    }
-    
-    vim.g.vimtex_compiler_auto_compile = true
-    
-    -- 优化 Skim 配置
-    vim.g.vimtex_view_method = 'skim'
-    vim.g.vimtex_view_skim_sync = 1  -- 启用同步
-    vim.g.vimtex_view_skim_activate = false  -- 避免频繁激活窗口
-    
-    -- 正向搜索配置
-    vim.g.vimtex_view_forward_search_on_startup = true
-    
-    -- 快捷键
-    vim.keymap.set('n', '<leader>ll', '<plug>(vimtex-compile)', { desc = '编译文档' })
-    vim.keymap.set('n', '<leader>lv', '<plug>(vimtex-view)', { desc = '打开 Skim 预览' })
-    vim.keymap.set('n', '<leader>lc', '<plug>(vimtex-clean)', { desc = '清理编译文件' })
-  end
+	"lervag/vimtex",
+	lazy = false,
+	init = function()
+		vim.g.vimtex_quickfix_mode = 2
+
+		vim.g.vimtex_compiler_latexmk_engines = {
+			_ = "-xelatex", -- 指定 latexmk 使用 xelatex 引擎 [citation:2]
+		}
+		vim.g.vimtex_compiler_latexmk = {
+			build_dir = "build",
+			options = {
+				"-pdfxe", -- 明确表示使用 xelatex 生成 PDF
+				"-synctex=1",
+				"-interaction=nonstopmode",
+				"-file-line-error",
+				"-shell-escape", -- 可选
+				"-verbose", -- 可选
+			},
+		}
+		-- 问ai问出来的，似乎没啥用
+		vim.g.vimtex_compiler_auto_compile = true
+
+		-- Skim 查看器配置
+		vim.g.vimtex_view_method = "skim"
+		vim.g.vimtex_view_skim_sync = 1
+		vim.g.vimtex_view_skim_activate = false
+
+		-- 快捷键映射
+		vim.keymap.set("n", "<leader>ll", "<plug>(vimtex-compile)", { desc = "编译文档" })
+		vim.keymap.set("n", "<leader>lv", "<plug>(vimtex-view)", { desc = "打开 Skim 预览" })
+		vim.keymap.set("n", "<leader>lc", "<plug>(vimtex-clean)", { desc = "清理编译文件" })
+	end,
 }
